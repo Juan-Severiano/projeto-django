938af3be146c8728d822bf87804ef14403665283
Juan Severiano <juansoussev@gmail.com>
2022-11-08 11:50:24 -0300

    Adicionei as parciais

--- app/views.py
+++ app/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render (request, 'app/index.html' , context={
+    return render (request, 'app/pages/index.html' , context={
         'name':'Juan Severiano'
     })
