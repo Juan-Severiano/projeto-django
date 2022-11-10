df0c9baac2ab7c8cd1426a7c358a17d8239b1f7a
Juan Severiano <juansoussev@gmail.com>
2022-11-10 17:40:03 -0300

    Faker adicionado ao site de teste

--- app/views.py
+++ app/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
     return render (request, 'app/pages/index.html' , context={
-        'name':'Juan Severiano'
+        'recipes':[make_recipe() for _ in range(10)],
     })
 
 def recipes(request, id):
     return render (request, 'app/pages/recipe-view.html' , context={
-        'name':'Juan Severiano'
+        'recipe':make_recipe()
     })
