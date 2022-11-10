d983e3e2c771d0069b278786c0f8ad1809aae1e3
Juan Severiano <juansoussev@gmail.com>
2022-11-10 20:29:54 -0300

    Adicionei minhas migrações

--- app/models.py
+++ app/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 class Recipes(models.Model):
-    title = models.CharField(max_length=65)
+    name = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
@@ -16,4 +20,5 @@ class Recipes(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
-    category  = 
+    category  = models.ForeignKey (Category, on_delete=models.SET_NULL, null=True )
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
