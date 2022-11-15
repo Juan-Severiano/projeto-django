132655970a01da136390d1a5a579f87a90d670ce
Juan Severiano <juansoussev@gmail.com>
2022-11-14 22:08:00 -0300

    ...

--- app/models.py
+++ app/models.py
@@ -6,8 +6,11 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
-class Recipes(models.Model):
-    name = models.CharField(max_length=65)
+    def  __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
@@ -19,6 +22,9 @@ class Recipes(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
-    category  = models.ForeignKey (Category, on_delete=models.SET_NULL, null=True )
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    def __str__(self):
+        return self.title
